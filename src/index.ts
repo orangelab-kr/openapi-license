6fd04230822b768ec0ba62f2c84232256b805637
bakjonghun <bakjonghun@hikick.kr>
2021-02-23 20:55:46 +0900

    add: joi & base URL

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import getRouter from './routes';
 import serverless from 'serverless-http';
 if (process.env.NODE_ENV === 'development') dotenv.config();
 
-export const handler = serverless(getRouter());
+const options = { basePath: '/v1/license' };
+export const handler = serverless(getRouter(), options);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,17 @@
 import express, { Application } from 'express';
 
 import InternalError from '../tools/error';
+import Joi from '../tools/joi';
+import License from '../controllers/license';
+import OPCODE from '../tools/opcode';
 import Wrapper from '../tools/wrapper';
-import getLicenseRouter from './license';
 import logger from '../tools/logger';
 import morgan from 'morgan';
+import os from 'os';
 
 export default function getRouter(): Application {
   const router = express();
+  const hostname = os.hostname();
   const logging = morgan('common', {
     stream: { write: (str: string) => logger.info(`${str.trim()}`) },
   });
@@ -15,7 +19,41 @@ export default function getRouter(): Application {
   router.use(logging);
   router.use(express.json());
   router.use(express.urlencoded({ extended: true }));
-  router.use('/v1/license', getLicenseRouter());
+
+  router.get(
+    '/',
+    Wrapper(async (_req, res) => {
+      res.json({
+        opcode: OPCODE.SUCCESS,
+        mode: process.env.NODE_ENV,
+        cluster: hostname,
+      });
+    })
+  );
+
+  router.post(
+    '/',
+    Wrapper(async (req, res) => {
+      const schema = Joi.object({
+        realname: Joi.string().min(2).max(10).required(),
+        birthday: Joi.date().required(),
+        identity: Joi.string().length(6).required(),
+        license: Joi.array()
+          .required()
+          .items(
+            Joi.string().length(2).required(),
+            Joi.string().length(2).required(),
+            Joi.string().length(6).required(),
+            Joi.string().length(2).required()
+          ),
+      });
+
+      const options = { abortEarly: false };
+      const data = await schema.validateAsync(req.body, options);
+      const isValid = await License.isValidLicense(data);
+      res.json({ opcode: OPCODE.SUCCESS, isValid });
+    })
+  );
 
   router.all(
     '*',
